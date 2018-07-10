90fb4a518d399e35f468b8681d69a64ee1230e07
ExoWanderer <jdfraine@gmail.com>
2018-07-10 18:05:27 -0400

    Reworked directory structure

---
+++ MeGGNet16/jdf_smallervggnet.py
@@ -0,0 +1,68 @@
+# Keras Implementation of a Light VGG16 DLN from https://www.pyimagesearch.com/2018/04/16/keras-and-convolutional-neural-networks-cnns/
+# import the necessary packages
+from keras.models import Sequential
+from keras.layers.normalization import BatchNormalization
+from keras.layers.convolutional import Conv2D
+from keras.layers.convolutional import MaxPooling2D
+from keras.layers.core import Activation
+from keras.layers.core import Flatten
+from keras.layers.core import Dropout
+from keras.layers.core import Dense
+from keras import backend as K
+
+class SmallerVGGNet:
+    @staticmethod
+    def build(width, height, depth, classes):
+        # initialize the model along with the input shape to be
+        # "channels last" and the channels dimension itself
+        model = Sequential()
+        inputShape = (height, width, depth)
+        chanDim = -1
+        
+        # if we are using "channels first", update the input shape
+        # and channels dimension
+        if K.image_data_format() == "channels_first":
+            print("Not Using TensorFlow")
+            inputShape = (depth, height, width)
+            chanDim = 1
+        
+        # CONV => eLU => POOL
+        model.add(Conv2D(32, (3, 3), padding="same", input_shape=inputShape))
+        model.add(Activation("elu"))
+        model.add(BatchNormalization(axis=chanDim))
+        model.add(MaxPooling2D(pool_size=(3, 3)))
+        model.add(Dropout(0.25))
+        
+        # (CONV => eLU) * 2 => POOL
+        model.add(Conv2D(64, (3, 3), padding="same"))
+        model.add(Activation("elu"))
+        model.add(BatchNormalization(axis=chanDim))
+        model.add(Conv2D(64, (3, 3), padding="same"))
+        model.add(Activation("elu"))
+        model.add(BatchNormalization(axis=chanDim))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Dropout(0.25))
+        
+        # (CONV => eLU) * 2 => POOL
+        model.add(Conv2D(128, (3, 3), padding="same"))
+        model.add(Activation("elu"))
+        model.add(BatchNormalization(axis=chanDim))
+        model.add(Conv2D(128, (3, 3), padding="same"))
+        model.add(Activation("elu"))
+        model.add(BatchNormalization(axis=chanDim))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Dropout(0.25))
+        
+        # first (and only) set of FC => eLU layers
+        model.add(Flatten())
+        model.add(Dense(1024))
+        model.add(Activation("elu"))
+        model.add(BatchNormalization())
+        model.add(Dropout(0.5))
+        
+        # softmax classifier
+        model.add(Dense(classes))
+        model.add(Activation("softmax"))
+        
+        # return the constructed network architecture
+        return model
