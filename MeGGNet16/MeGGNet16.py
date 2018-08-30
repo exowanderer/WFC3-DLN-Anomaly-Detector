9c853cc88eef2eed7b1d03dc68f0853fbc4328d2
Jonathan Fraine <exowanderer@users.noreply.github.com>
2018-08-30 03:28:11 -0400

    rempved unnecessary imports

--- MeGGNet16/MeGGNet16.py
+++ MeGGNet16/MeGGNet16.py
@@ -1,8 +1,8 @@
 from keras.models import Sequential
 from keras.layers.core import Flatten, Dense, Dropout
 from keras.layers.convolutional import Conv2D, MaxPooling2D, ZeroPadding2D
-from keras.optimizers import SGD
-import cv2, numpy as np
+
+import numpy as np
 
 from keras.layers.normalization import BatchNormalization
 from keras import backend as K
