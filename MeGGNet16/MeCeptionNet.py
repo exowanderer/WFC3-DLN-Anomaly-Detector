d7993c178d6681fa7334d6d906644fc4980fd812
Jonathan Fraine <exowanderer@users.noreply.github.com>
2018-08-30 03:28:35 -0400

    removed unnecassary imports

--- MeGGNet16/MeCeptionNet.py
+++ MeGGNet16/MeCeptionNet.py
@@ -2,9 +2,8 @@ from keras.models import Sequential
 from keras.layers.merge import Add
 from keras.layers.core import Flatten, Dense, Dropout, Input
 from keras.layers.convolutional import Conv2D, MaxPooling2D, ZeroPadding2D. AveragePooling2D
-from keras.optimizers import SGD
 
-import cv2, numpy as np
+import numpy as np
 
 from keras.layers.normalization import BatchNormalization
 from keras import backend as K
